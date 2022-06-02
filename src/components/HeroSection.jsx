e8063b5289dd956b1ed34fb51baa4331a3e5c4cc
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-02 14:07:50 -0500

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
     <div id='hero' className='flex items-center justify-center flex-col py-20'>
         <div className="text-center">
             <h1 className='text-2xl md:text-4xl mb-1 md:mb-3 
-            text-indigo-600 font-semibold dark:text-indigo-500 sd:font-sans'>Hi, My Name 0is Isaac</h1>
+            text-indigo-600 font-semibold dark:text-indigo-500 sd:font-sans'>Hi, My Name is Isaac</h1>
             <p className='text-md md:text-xl max-w-md
               mb-3 text-gray-600 dark:text-gray-300'>
               “Intelligence is not a privilege, it's a gift. 
