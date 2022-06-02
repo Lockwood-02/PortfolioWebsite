9e0f23c6a1fd2e83c26f50883a19ae771746636f
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-02 15:19:17 -0500

    Update works.js

--- src/data/works.js
+++ src/data/works.js
@@ -2,6 +2,7 @@ export default [
     {
         title: 'ToDo List',
         imgUrl: 'https://repository-images.githubusercontent.com/499035921/1509eac1-d536-4f85-83bd-eab25226d53d',
+        description: 'test text',
         tech:['HTML', 'JavaScript'],
         workUrl: 'http://todo-app-website.s3-website.us-east-2.amazonaws.com/'
     },
@@ -9,6 +10,7 @@ export default [
     {
         title: 'Work In Progress',
         imgUrl: 'https://repository-images.githubusercontent.com/499037818/3f0faea0-5081-4e96-a365-6a8d22879da1',
+        description: 'test text',
         tech:['HTML', 'CSS'],
         workUrl: 'https://github.com/Lockwood-02'
     },
@@ -16,6 +18,7 @@ export default [
     {
         title: 'Work In Progress',
         imgUrl: 'https://repository-images.githubusercontent.com/499037818/3f0faea0-5081-4e96-a365-6a8d22879da1',
+        description: 'test text',
         tech:['HTML', 'CSS'],
         workUrl: 'https://github.com/Lockwood-02'
     },
@@ -23,6 +26,7 @@ export default [
     {
         title: 'Work In Progress',
         imgUrl: 'https://repository-images.githubusercontent.com/499037818/3f0faea0-5081-4e96-a365-6a8d22879da1',
+        description: 'test text',
         tech:['HTML', 'ReactJS'],
         workUrl: 'https://github.com/Lockwood-02'
     }
