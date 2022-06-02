00d13532b338f55b1e451b396001675bffe63609
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-02 16:07:53 -0500

    Update WorkItem.jsx

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -8,7 +8,7 @@ function WorkItem({imgUrl, title, description, tech, workUrl}) {
         className="w-full h-36 md:h-48 object-cover"/>
         <div className='text-gray-600 dark:text-gray-300 p-5 w-full'>
             <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
-            <p>
+            <p className='mb-3'>
               {description}
             </p>
             <p className='flex flex-wrap gap-2 flex-row 
