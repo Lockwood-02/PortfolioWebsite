80dbf2b2eb54c985a556155784aee140007c81cc
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 16:22:02 -0500

    Update footer.jsx

--- src/data/footer.jsx
+++ src/data/footer.jsx
@@ -1,7 +1,12 @@
 import { TbBrandLinkedin } from 'react-icons/tb'
+import { TbBrandGithub } from 'react-icons/tb'
 
 export default [
   {
       icon: <TbBrandLinkedin className='w-full h-full'></TbBrandLinkedin>
-  }
+  },
+  
+  {
+      icon: <TbBrandGithub className='w-full h-full'></TbBrandGithub>
+  },
 ]
