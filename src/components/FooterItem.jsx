b5d527da76ed531477632258e8517f1c65f7541e
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 16:37:39 -0500

    Update FooterItem.jsx

--- src/components/FooterItem.jsx
+++ src/components/FooterItem.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function FooterItem({icon, footerUrl}) {
   return (
-    <a href={footerUrl} target='_blank' rel='noreferrer'
+    <a href={footerUrl} target='_blank' rel='noreferrer'>
        <div className='text-gray-300 dark:text-gray-300 w-6 h-6 mb-3'>
            {icon}
        </div>
-    ></a>
+    </a>
   )
 }
 
