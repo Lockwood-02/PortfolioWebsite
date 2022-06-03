df6129103b0bd55d08d0a726be79b561d0da97e8
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 15:57:04 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,16 @@ function Footer() {
     <div className='py-5 bg-slate-800 text-center text-gray-300
         rounded-t-lg'>
         <a href="#hero" className='block text-xl md:text-2xl font-semibold'>Isaac Lockwood</a>
-        <a href="mailto:isaaclockwood618@gmail.com"
-            className=' text-sm md:text-md hover:text-indigo-500'
+        <a href="mailto:isaaclockwood618@gmail.com" className=' text-sm md:text-md hover:text-indigo-500'
         >isaaclockwood618@gmail.com</a>
-        {footer.map(footers => (
-          <FooterItem
-          key={footers.icon}
-          icon={footers.icon}
-          ></FooterItem>
-        ))}
+        <div className='flex justify-center'>
+          {footer.map(footers => (
+            <FooterItem
+            key={footers.icon}
+            icon={footers.icon}
+            ></FooterItem>
+          ))}
+        </div>
         <p className='text-xs mt-2 text-gray-500'>
             © Isaac Lockwood {new Date().getFullYear()}. All rights reserved.
         </p>
