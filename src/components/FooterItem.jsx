cb290330d9084e6573c5f356cb4cf1dbdda75434
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 15:09:23 -0500

    Create FooterItem.jsx

---
+++ src/components/FooterItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FooterItem({icon}) {
+  return (
+       <div className='text-gray-800 dark:text-gray-300 w-6 h-6 mb-3'>
+           {icon}
+       </div>
+  )
+}
+
+export default ServiceItem
