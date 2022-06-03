cc0c41750761ef1b671ade0ff24564740ec4c75d
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 16:03:48 -0500

    Update FooterItem.jsx

--- src/components/FooterItem.jsx
+++ src/components/FooterItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FooterItem({icon}) {
   return (
-       <div className='text-gray-800 dark:text-gray-300 w-6 h-6 mb-3'>
+       <div className='text-gray-300 dark:text-gray-300 w-6 h-6 mb-3'>
            {icon}
        </div>
   )
