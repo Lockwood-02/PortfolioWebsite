26f874f486c9d21d302e9f705f43bb2ba2fbeb6e
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-03 20:50:13 -0500

    Update footer.jsx

--- src/data/footer.jsx
+++ src/data/footer.jsx
@@ -1,6 +1,7 @@
 import { TbBrandLinkedin } from 'react-icons/tb'
 import { TbBrandGithub } from 'react-icons/tb'
 import { TbBrandInstagram } from 'react-icons/tb'
+import { TbBrandTwitter } from 'react-icons/tb'
 
 export default [
   {
@@ -16,5 +17,10 @@ export default [
   {
       icon: <TbBrandInstagram className='w-full h-full'></TbBrandInstagram>,
       footerUrl: 'https://www.instagram.com/isaac_lockwood/?hl=en'
+  },
+  
+  {
+      icon: <TbTbBrandTwitter className='w-full h-full'></TbBrandTbBrandTwitter>,
+      footerUrl: 'https://twitter.com/isaaclockwooood'
   },
 ]
