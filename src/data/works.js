3b336e0c6b3b1243f1aea87df6d2d538837ee2d4
Juno <78705202+Lockwood-02@users.noreply.github.com>
2022-06-12 16:43:06 -0500

    Update works.js

--- src/data/works.js
+++ src/data/works.js
@@ -4,7 +4,8 @@ export default [
         imgUrl: 'https://repository-images.githubusercontent.com/499035921/1509eac1-d536-4f85-83bd-eab25226d53d',
         description: 'One of my first projects I completed. A simple ToDo App that was a starting point into my career.',
         tech:['HTML', 'JavaScript'],
-        workUrl: 'http://todo-app-website.s3-website.us-east-2.amazonaws.com/'
+        workUrl: 'http://todo-app-website.s3-website.us-east-2.amazonaws.com/',
+        source: 'https://github.com/Lockwood-02/ToDoApp'
     },
 
     {
@@ -12,22 +13,25 @@ export default [
         imgUrl: 'https://repository-images.githubusercontent.com/499298250/d2d3058f-0fc3-4c67-974f-01ee6de2fed7',
         description: 'A simple website outline that can be modified to fit any needs from a consumer.',
         tech:['HTML', 'CSS'],
-        workUrl: 'https://lockwood-02.github.io/BusinessSite/'
+        workUrl: 'https://lockwood-02.github.io/BusinessSite/',
+        source: 'https://github.com/Lockwood-02/BusinessSite'
     },
 
     {
         title: 'Work In Progress',
         imgUrl: 'https://repository-images.githubusercontent.com/499037818/3f0faea0-5081-4e96-a365-6a8d22879da1',
-        description: 'Work in progress',
+        description: 'Work in Progress',
         tech:['HTML', 'CSS'],
-        workUrl: 'https://github.com/Lockwood-02'
+        workUrl: 'https://github.com/Lockwood-02',
+        source: 'https://github.com/Lockwood-02'
     },
 
     {
         title: 'Work In Progress',
         imgUrl: 'https://repository-images.githubusercontent.com/499037818/3f0faea0-5081-4e96-a365-6a8d22879da1',
-        description: 'Work in progress',
+        description: 'Work in Progress',
         tech:['HTML', 'ReactJS'],
-        workUrl: 'https://github.com/Lockwood-02'
+        workUrl: 'https://github.com/Lockwood-02',
+        source: 'https://github.com/Lockwood-02'
     }
 ];
